00efa7feea2a68ea265a6c7c69d20cdffa7399c5
kennedysferreira <sferreira.kennedy@gmail.com>
2024-06-27 11:15:16 +0100

    animation finish

--- srcjs/main.js
+++ srcjs/main.js
@@ -1,15 +1,11 @@
-const handleMousemove = (e) => {
-  const cursor = document.getElementById('cursor');
-  
-  
-  const mouseX = e.clientX;
-  const mouseY = e.clientY;
-  
-  // Define as transformações necessárias
-  const transform = `translate(-50%, -50%) translate(${mouseX}px , ${mouseY}px)`;
-  
-  // Aplica as transformações ao cursor
-  cursor.style.transform = transform;
-}
+const cursor = document.getElementById("cursor");
 
-document.addEventListener('mousemove', handleMousemove);
+const handleCursorMove = (e) => {
+  const cursorX = e.clientX;
+  const cursorY = e.clientY;
+  const transform = `translate(-50%, -50%) translate(${cursorX}px , ${cursorY}px)`;
+  cursor.style.transform = transform;
+};
+
+document.addEventListener("mousemove", handleCursorMove);
+
