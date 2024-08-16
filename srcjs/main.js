ca99fbbd56a0678cb8f17736d6c7268da20d333a
kennedy <sferreira.kennedy@gmail.com>
2024-08-16 18:16:58 +0100

    fixed bug safari

--- srcjs/main.js
+++ srcjs/main.js
@@ -1,4 +1,6 @@
 const cursor = document.getElementById("cursor");
+const linkDownload = document.getElementById("downloadLink");
+const iFrame = document.getElementById("downloadFrame");
 
 const handleCursorMove = (e) => {
   const cursorX = e.clientX;
@@ -6,5 +8,10 @@ const handleCursorMove = (e) => {
   const transform = `translate(-50%, -50%) translate(${cursorX}px , ${cursorY}px)`;
   cursor.style.transform = transform;
 };
+const handleDownload = () => {
+  iFrame.src =
+    "https://github.com/kennedysferreira/uploadCV/raw/main/Cv%20-%20Kennedy%20Ferreira.pdf";
+};
 
 document.addEventListener("mousemove", handleCursorMove);
+linkDownload.addEventListener("click", handleDownload);
